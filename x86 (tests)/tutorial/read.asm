b8803f161ed327286496a41e9101c9b57ed001b8
Nacho Granados <ignaciogramar@gmail.com>
2022-03-13 21:07:12 -0600

    Creating new project with SASM IDE

--- x86 (tests)/tutorial/read.asm
+++ x86 (tests)/tutorial/read.asm
@@ -17,23 +17,23 @@ global  _start
  
 _start:
  
-    mov     ecx, 0777o          ; Create file from lesson 22
-    mov     ebx, filename
-    mov     eax, 8
-    int     80h
+;    mov     ecx, 0777o          ; Create file from lesson 22
+;    mov     ebx, filename
+;    mov     eax, 8
+;    int     80h
  
-    mov     edx, 12             ; Write contents to file from lesson 23
-    mov     ecx, contents
-    mov     ebx, eax
-    mov     eax, 4
-    int     80h
+;    mov     edx, 12             ; Write contents to file from lesson 23
+;    mov     ecx, contents
+;    mov     ebx, eax
+;    mov     eax, 4
+;    int     80h
  
     mov     ecx, 0              ; Open file from lesson 24
     mov     ebx, filename
     mov     eax, 5
     int     80h
  
-    mov     edx, 12             ; number of bytes to read - one for each letter of the file contents
+    mov     edx, 2000             ; number of bytes to read - one for each letter of the file contents
     mov     ecx, fileContents   ; move the memory address of our file contents variable into ecx
     mov     ebx, eax            ; move the opened file descriptor into EBX
     mov     eax, 3              ; invoke SYS_READ (kernel opcode 3)
--- x86 (tests)/tutorial/write.asm
+++ x86 (tests)/tutorial/write.asm
@@ -8,6 +8,7 @@
 SECTION .data
 filename db 'readme.txt', 0h    ; the filename to create
 contents db 'Hello world!', 0h  ; the contents to write
+ len: equ $-contents
  
 SECTION .text
 global  _start
@@ -19,7 +20,7 @@ _start:
     mov     eax, 8
     int     80h
  
-    mov     edx, 12             ; number of bytes to write - one for each letter of our contents string
+    mov     edx, len             ; number of bytes to write - one for each letter of our contents string
     mov     ecx, contents       ; move the memory address of our contents string into ecx
     mov     ebx, eax            ; move the file descriptor of the file we created into ebx
     mov     eax, 4              ; invoke SYS_WRITE (kernel opcode 4)
