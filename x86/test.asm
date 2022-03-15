5b12cb6f8cf3057428a8f5afbb875dc5b9f3fb77
Nacho Granados <ignaciogramar@gmail.com>
2022-03-14 20:37:04 -0600

    Adding Bresenham's algorithm

--- x86/test.asm
+++ x86/test.asm
@@ -7,12 +7,12 @@ global  _start
  
 _start:
  
-    mov     edx, len
-    mov     ecx, msg
-    mov     ebx, 1
-    mov     eax, 4
+    mov     rdx, len
+    mov     rcx, msg
+    mov     rbx, 1
+    mov     rax, 4
     int     80h
  
-    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
-    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    mov     rbx, 0      ; return 0 status on exit - 'No Errors'
+    mov     rax, 1      ; invoke SYS_EXIT (kernel opcode 1)
     int     80h
