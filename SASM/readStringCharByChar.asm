79d4260a86e13f214a925a297ef7df42a866c28a
Nacho Granados <ignaciogramar@gmail.com>
2022-03-16 00:02:02 -0600

    Testing Bresenham's algorithm with negative slopes

---
+++ SASM/readStringCharByChar.asm
@@ -0,0 +1,70 @@
+%include "io64.inc"
+
+section .data
+msg db 'Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello WorldHello World Hello World Hello World Hello World'
+
+len: equ $-msg
+
+space db 0Ah
+
+array times 15 dq ' ' ; max value => 5
+
+section .text
+global CMAIN
+
+CMAIN:
+    mov rbp, rsp; for correct debugging
+    
+    mov r15, len
+    
+    mov r8, 0 ; cont
+    
+    mov r10, 0 ; row
+    
+_loop:
+
+    mov r9, msg ; msg addres
+    add r9, r8
+ 
+    ; print one letter
+    mov     rdx, 1
+    mov     rcx, r9
+    mov     rbx, 1
+    mov     rax, 4
+    int     80h
+    
+    add r10, 1
+    
+    cmp r10, 41
+    jl _continue 
+    
+    mov r10, 0  
+    
+    ; print new line
+    mov     rdx, 1
+    mov     rcx, space
+    mov     rbx, 1
+    mov     rax, 4
+    int     80h
+    
+_continue:
+    
+    add r8, 1
+    
+    cmp r8, len
+    jl _loop
+     
+_exit:
+    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
+    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    int     80h
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- SASM/test.asm
+++ SASM/test.asm
@@ -1,9 +1,6 @@
-%include "io.inc"
+%include "io64.inc"
 
-section .data
-
-arrayX times 15 dq 0
-arrayY times 15 dq 0
+%include "functions.asm"
 
 section .text
 global CMAIN
@@ -15,61 +12,14 @@ CMAIN:
     mov rsi, 2
     mov rdx, 15
     mov rcx, 5
-
-; rdi = x1
-; rsi = y1
-; rdx = x2 
-; rcx = y2 
-_bresenham:
-    mov rax, rcx ; rax = y2
-    sub rax, rsi ; rax = y2 - y1
-    imul rax, 2 ; rax = 2 * (y2 - y1) => m_new
-
-    mov rbx, rdx ; rbx = x2
-    sub rbx, rdi ; rbx = x2 - x1
-    mov r8, rax ; r8 = m_new
-    sub r8, rbx ; r8 = m_new - (x2 - x1) => slope_error_new
-    mov rbx, r8 ; rbx = m_new - (x2 - x1) => slope_error_new
-
-    mov r8, 0 ; r8 = arrayX and arrayY pointer => offset
-    mov r9, arrayX ; r9 = arrayX address
-    mov r10, arrayY ; r10 = arrayY address
-    mov r11, rdi ; r11 = x1 => x
-    mov r12, rsi ; r12 = y1 => y
-
-_bresenham_for_loop:
-    cmp r11, rdx ; x <= x2 ?
-    jg _bresenham_exit ; x > x2
-
-    mov [r9 + r8], r11 ; arrayX[i] = x
-    mov [r10 + r8], r12 ; arrayX[i] = y
-
-    add r8, 4 ; ofset++
-
-    add rbx, rax ; slope_error_new += m_new
-
-_bresenham_if:
-    cmp rbx, 0 ; slope_error_new >= 0 ?
-    jl _bresenham_continue ; slope_error_new < 0
-
-    add r12, 1 ; y++
-
-    mov r13, rdx ; r13 = x2
-    sub r13, rdi ; r13 = x2 - x1
-    imul r13, 2 ; r13 = 2 * (x2 - x1)
-    sub rbx, r13 ; rbx -= 2 * (x2 - x1) => slope_error_new
     
-_bresenham_continue:
-    add r11, 1 ; x++
+    call quit
 
-    jmp _bresenham_for_loop ; for loop
+;_exit:
 
-_bresenham_exit:
-
-    mov     rbx, 0
-    mov     rax, 1
-    int     80h
-    ret
+ ;   mov     rbx, 0
+  ;  mov     rax, 1
+   ; int     80h
 
 
 
